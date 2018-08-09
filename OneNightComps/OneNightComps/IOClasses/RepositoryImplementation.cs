99f4102f90a39402a8318887f807239c75e84a70
findusl <sebastian@lehrbaum.de>
2018-08-09 23:01:19 +0200

    Added server access functionality

---
+++ OneNightComps/OneNightComps/IOClasses/RepositoryImplementation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OneNightComps.Model;
+
+namespace OneNightComps.IOClasses
+{
+    class RepositoryImplementation : IGameCompositionRepository, IGameRoleRepository
+    {
+        User user;
+        ServerClient client;
+
+        public RepositoryImplementation(User user)
+        {
+            this.user = user;
+            client = new ServerClient(user);
+        }
+
+        public async Task<List<GameRole>> GetRoles()
+        {
+            List<GameRole> result = await client.GetString<List<GameRole>>("one_night_comps/roles/read.php");
+            return result;
+        }
+
+        public Task<bool> AddGameComoposition(GameComposition composition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GameComposition>> GetGameCompositions()
+        {
+            List<GameComposition> result = await client.GetString<List<GameComposition>>("one_night_comps/compositions/read.php");
+            return result;
+        }
+
+        public Task<List<GameComposition>> GetGameCompositions(GameCompositionSpecification specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OneNightComps/OneNightComps/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneNightComps.Model
+{
+    class User
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string E_Mail {get; set;}
+        public string Token { get; set; }
+    }
+}
