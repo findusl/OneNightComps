3303d04a762009837139ad116fadbd9a6cfc5d6c
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2018-08-09 18:16:26 +0200

    Renamed and added some php files

---
+++ Backend/database.php
@@ -0,0 +1,43 @@
+<?php
+mysqli_report(MYSQLI_REPORT_STRICT);
+class Database{
+
+    $currentVersion = 1.0;//is there some way to make constant?
+ 
+    // specify your own database credentials
+    private $host = "rdbms.strato.de";
+    private $username = "U2285767";
+    private $password = "W3oovyYr56LZt9wIOHUr";
+    private $db_name = "DB2285767";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            echo "Trying to establish connection<br>";
+            $db = new mysqli($host, $username, $password, $db_name);
+
+            if($db->connect_errno > 0){
+                exit('Unable to connect to database [' . $db->connect_error . ']');
+            }
+
+            echo "Connected to database<br>"
+        }catch(mysqli_sql_exception $exception){
+            exit("Connection error: " . $exception->getMessage());
+        }
+ 
+        return $db;
+    }
+
+    public function getJSON($result) {
+        $resultArray = array();
+        while($row = $result->fetch_array(MYSQL_ASSOC)) {
+            $resultArray[] = $row;
+        }
+        return json_encode($resultArray);
+    }
+}
+?>
