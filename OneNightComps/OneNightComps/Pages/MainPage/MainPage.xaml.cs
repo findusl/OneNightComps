375266fc68143665c9be744a2f0372e485c80bdd
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2018-08-11 20:20:16 +0200

    Added empty pages for the start page

---
+++ OneNightComps/OneNightComps/Pages/MainPage/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OneNightComps.Pages.MainPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+			Master = new MenuPage();
+			Detail = new SearchPage();
+		}
+	}
+}
