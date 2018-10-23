db53a57485d8680d4dff5fc4cc20d03ab167dc0a
findusl <sebastian@lehrbaum.de>
2018-10-23 21:51:06 +0200

    small change

--- Android/app/src/main/java/lehrbaum/de/onenightcomps/view_models/CompositionsListViewModel.kt
+++ Android/app/src/main/java/lehrbaum/de/onenightcomps/view_models/CompositionsListViewModel.kt
@@ -1,8 +1,14 @@
 package lehrbaum.de.onenightcomps.view_models
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import lehrbaum.de.onenightcomps.data_access.CompositionRepository
+import lehrbaum.de.onenightcomps.model.Composition
 
 class CompositionsListViewModel : ViewModel() {
-
+    val compositionRepository = CompositionRepository;
+    val compositionList: LiveData<List<Composition>> by lazy {
+        MutableLiveData<List<Composition>>()
+    }
 }
