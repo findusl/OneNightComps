60675d969d58ca99ac3bc9df6258aeebbcf38b49
Sebastian Lehrbaum <sebastian@lehrbaum.de>
2019-02-22 10:36:30 +0100

    Renamed Fragments to fit standard naming scheme

--- Android/app/src/main/java/lehrbaum/de/onenightcomps/fragments/CompositionDetailFragment.kt
+++ Android/app/src/main/java/lehrbaum/de/onenightcomps/fragments/CompositionDetailFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
-import lehrbaum.de.onenightcomps.LiveEvent
-import lehrbaum.de.onenightcomps.databinding.CompositionDetailBinding
+import lehrbaum.de.onenightcomps.databinding.FragmentCompositionDetailBinding
 import lehrbaum.de.onenightcomps.viewmodel.AppViewModel
 
 class CompositionDetailFragment : Fragment() {
@@ -16,7 +14,7 @@ class CompositionDetailFragment : Fragment() {
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		val binding = CompositionDetailBinding.inflate(inflater, container, false)
+		val binding = FragmentCompositionDetailBinding.inflate(inflater, container, false)
 		binding.lifecycleOwner = this
 		binding.composition = AppViewModel.selectedComposition.value
 		binding.rolesView.roleClickListener = {
